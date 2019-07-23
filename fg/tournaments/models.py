d186669923deff3772cb8d3766389e866dca04aa
alex-evans <alex.evans.d@gmail.com>
2019-07-22 21:27:10 -0500

    Initial build of tournaments, rough start.

--- fg/tournaments/models.py
+++ fg/tournaments/models.py
@@ -5,9 +5,54 @@ class Tournament(models.Model):
     name = models.CharField(max_length=200)
     start_date = models.DateTimeField('start date')
     end_date = models.DateTimeField('end date')
+    
+    def __str__(self):
+        return self.name
+
+
+class GroupAPlayer(models.Model):
+    tournament = models.ForeignKey(Tournament, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    winnings = models.IntegerField()
+
+    def __str__(self):
+        return self.tournament + " - Group A: " + self.name
+
+
+class GroupBPlayer(models.Model):
+    tournament = models.ForeignKey(Tournament, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    winnings = models.IntegerField()
+
+    def __str__(self):
+        return self.tournament + " - Group B: " + self.name
+
+
+class GroupCPlayer(models.Model):
+    tournament = models.ForeignKey(Tournament, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    winnings = models.IntegerField()
+
+    def __str__(self):
+        return self.tournament + " - Group C: " + self.name
+
+
+class GroupDPlayer(models.Model):
+    tournament = models.ForeignKey(Tournament, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    winnings = models.IntegerField()
+
+    def __str__(self):
+        return self.tournament + " - Group D: " + self.name
 
 
 class Person(models.Model):
     tournament = models.ForeignKey(Tournament, on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
-    
+    pick_a = models.ForeignKey(GroupAPlayer, on_delete=models.CASCADE)
+    pick_b = models.ForeignKey(GroupBPlayer, on_delete=models.CASCADE)
+    pick_c = models.ForeignKey(GroupCPlayer, on_delete=models.CASCADE)
+    pick_d = models.ForeignKey(GroupDPlayer, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.tournament + " - " + self.name
--- fg/tournaments/views.py
+++ fg/tournaments/views.py
@@ -1,5 +1,14 @@
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+
+from .models import Tournament
 
 
 def index(request):
-    return HttpResponse("Hello tournaments!")
+    latest_tournament_list = Tournament.objects.order_by('-start_date')[:20]
+    context = {'latest_tournament_list': latest_tournament_list}
+    return render(request, 'tournaments/index.html', context)
+
+
+def detail(request, tournament_id):
+    tournament = get_object_or_404(Tournament, pk=tournament_id)
+    return render(request, 'tournaments/detail.html', {'tournament': tournament})
