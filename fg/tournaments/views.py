cb026727c036eac92c5a95a574145a806649f1d7
alex-evans <alex.evans.d@gmail.com>
2019-08-02 06:41:53 -0500

    Updated to pull total winnings for Tournaments

--- fg/tournaments/views.py
+++ fg/tournaments/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404, render
-from .models import Tournament, TournamentPick
-from django.db.models import Sum
+from .models import Tournament, TournamentPick, Player
+from bs4 import BeautifulSoup
+import requests
 
 
 def index(request):
@@ -11,5 +12,35 @@ def index(request):
 
 def detail(request, tournament_id):
     tournament = get_object_or_404(Tournament, pk=tournament_id)
+    update_winnings(tournament)
     picks_obj = TournamentPick.objects.filter(tournament=tournament).order_by('total_winnings')
     return render(request, 'tournaments/detail.html', {'tournament': tournament, 'picks': picks_obj})
+
+
+def update_winnings(tournament):
+    url = tournament.leaderboard_url
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    field = soup.find_all('tr',{'class':'Table2__tr Table2__even'})
+    players = []
+    for row in field:
+        name = row.findAll('td')[1].get_text()
+        player_obj = get_player_obj(name)
+        winnings = format_winnings(row.findAll('td')[8].get_text())
+        players.append([player_obj,winnings])
+    print(players)
+
+
+def get_player_obj(name):
+    try:
+        return Player.objects.get(name=name)
+    except Player.DoesNotExist:
+        print(f"ERROR: Player not found {name}")
+
+
+def format_winnings(winnings):
+        winnings = winnings.replace("$","").replace(",","")
+        if winnings == "--":
+            winnings = 0
+        return int(winnings)
+        
